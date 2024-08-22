7908c0f86a6918812b204d65ce14b58d53467563
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-22 06:19:36 +0530

    chore: Add constants for game objects, sounds, and backgrounds

---
+++ app/src/constants/index.ts
@@ -0,0 +1,37 @@
+// Objects
+import blackBall from "/assets/objects/blackBall.png";
+import redBall from "/assets/objects/redBall.png";
+import yellowBall from "/assets/objects/yellowBall.png";
+import whiteBall from "/assets/objects/whiteBall.png";
+
+import cueStick from "/assets/objects/cueStick.png";
+
+import GreenPool from "/assets/objects/pool-green.png";
+import RedPool from "/assets/objects/pool-red.png";
+
+import bg1 from "/assets/objects/main-bg.png";
+import bg2 from "/assets/objects/main-bg.jpg";
+
+// Sounds
+import BallsCollisionSound from "/assets/sounds/BallsCollide.wav";
+
+import HoleSound from "/assets/sounds/Hole.wav";
+import StrikeSound from "/assets/sounds/Strike.wav";
+
+
+
+// Exports
+export {
+  blackBall,
+  redBall,
+  yellowBall,
+  whiteBall,
+  cueStick,
+  GreenPool,
+  RedPool,
+  bg1,
+  bg2,
+  BallsCollisionSound,
+  HoleSound,
+  StrikeSound,
+};
