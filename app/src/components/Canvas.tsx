cedcc67fd720c290199a0dcb58e197222615cb58
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-22 06:20:39 +0530

    chore: Update Canvas2D component to use GreenPool constant for image source, Add relevant comments for better code readability

--- app/src/components/Canvas.tsx
+++ app/src/components/Canvas.tsx
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React, { useRef, useEffect } from "react";
-
+import { GreenPool } from "../constants";
+// Create a 2D canvas 
 const Canvas2D: React.FC = () => {
+  // Reference of HTMLCanvasElement
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
+  
+  // Function to draw image or render image on Canvas
   const drawImage = (
+    // props with types defined
     context: CanvasRenderingContext2D,
     image: HTMLImageElement,
     pos: { x: number; y: number },
     width: number,
     height: number
   ) => {
+    // canvas drawImage() function with passed arguments/parameters to drawImage on canvas with given (x,y) position and width,height
     context.drawImage(image, pos.x, pos.y, width, height);
   };
 
@@ -34,7 +39,7 @@ const Canvas2D: React.FC = () => {
     if (!gameCanvasContext) return;
 
     const image = new Image();
-    image.src = "/assets/sprites/spr_background4.png";
+    image.src = GreenPool;
     image.onload = () => {
       drawImage(
         gameCanvasContext,
